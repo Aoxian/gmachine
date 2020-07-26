1f681e93edccd366166994e8ec620f2713d09365
iang <iang@IanGs-MacBook-Pro.local>
2020-07-25 22:08:32 -0400

    Complete 1: Welcome to the Machine

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,15 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+type gmachineMemory struct {
+	 P  uint64
+	 Memory [DefaultMemSize]uint64
+}
+
+func New() *gmachineMemory {
+	return &gmachineMemory{
+		P: 0,
+		Memory: [DefaultMemSize]uint64{1:DefaultMemSize-1},
+	}
+}
